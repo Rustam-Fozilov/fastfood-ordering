d4e4f27dcd54047d594b3ed26d6ff10ec02c70c5
Rustam-Fozilov <fozilovrustam32@gmail.com>
2022-04-17 14:45:46 +0500

    KFC

---
+++ resources/views/kfc/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>KFC Tashkent</title>
+</head>
+
+<body>
+
+    <div id="header-wrapper">
+        <header class="header">
+            <div class="header-home">
+                <img src="{{URL::asset('img/kfc-logo.jpg')}}" alt="kfc-logo" id="logo-img">
+            </div>
+
+            <div id="main-menu">
+                <ul class="menu-item-ul">
+                    <li id="menu1"><a href="#basketi">Basketi</a></li> <!-- "a" - basketi dgan id li elementga boradi-->
+                    <li id="menu2"><a href="#basketi">Burgeri</a></li>
+                    <li id="menu3"><a href="#basketi">Tvisteri</a></li>
+                    <li id="menu4"><a href="#basketi">Kuritsa</a></li>
+                    <li id="menu5"><a href="#basketi">Kartofel fri</a></li>
+                    <li id="menu6"><a href="#basketi">sousi</a></li>
+                    <li id="menu7"><a href="#basketi">napitki</a></li>
+                    <li id="menu8"><a href="#basketi">Deserti</a></li>
+                </ul>
+            </div>
+        </header>
+    </div>
+
+</body>
+
+</html>
