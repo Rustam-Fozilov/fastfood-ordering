e5a2bf1b308a276cacc74fe95217fd6dd2e503d5
Rustam-Fozilov <fozilovrustam32@gmail.com>
2022-05-19 20:01:52 +0500

    menu added

---
+++ resources/views/fastfood/manageFood.blade.php
@@ -0,0 +1,15 @@
+@include('fastfood/menu')
+
+
+<!-- Main section starts here -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>MANAGE FOOD</h1>
+
+    </div>
+</div>
+<!-- Main section ends here -->
+
+</body>
+
+</html>
